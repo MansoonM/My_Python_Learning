c712d8c3d461801ff42d75364cb960517806d149
Mansoon Mohanty <136214687+MansoonM@users.noreply.github.com>
2025-04-10 06:57:47 +0530

    Add files via upload

---
+++ 5_numbers.py
@@ -0,0 +1,17 @@
+'''
+There are three numeric types in Python:
+
+int
+float
+complex
+
+'''
+
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+print(type(x))
+print(type(y))
+print(type(z))
