d67b497e5b0576828c8668ed5018ace85c918e6c
Mansoon Mohanty <136214687+MansoonM@users.noreply.github.com>
2025-04-10 06:57:31 +0530

    Add files via upload

---
+++ 4_variables.py
@@ -0,0 +1,19 @@
+# single variable
+a= 24
+b="Mansoon"
+print(a)
+print(b)
+
+
+# assigning multiple variables
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+# unpack a list
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
