eefd1c41d55d5bb93885cc11170278361996ef0d
Mansoon Mohanty <136214687+MansoonM@users.noreply.github.com>
2025-04-11 06:48:39 +0530

    Add files via upload

---
+++ 6_casting.py
@@ -0,0 +1,17 @@
+a = int(1)   # a will be 1
+b = int(2.8) # b will be 2
+c = int("3") # c will be 3
+
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+m = str("s1") # m will be 's1'
+n = str(2)    # n will be '2'
+o = str(3.0)  # o will be '3.0'
+
+
+print(a, b, c)
+print(x, y, z, w)
+print(m, n, o)
