8148517d1c724a2a7c2d1b7f43e4b304beb7dafd
Mansoon Mohanty <136214687+MansoonM@users.noreply.github.com>
2025-04-11 06:49:00 +0530

    Add files via upload

---
+++ 7_strings.py
@@ -0,0 +1,13 @@
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+
+
+# write a poem as it is
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+
+print(a)
