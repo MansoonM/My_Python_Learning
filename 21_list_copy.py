a0734787405740b6cd37e4470b51ab5dbb6a2c8c
Mansoon Mohanty <136214687+MansoonM@users.noreply.github.com>
2025-04-16 06:42:52 +0530

    Add files via upload

---
+++ 21_list_copy.py
@@ -0,0 +1,10 @@
+words=["moon","sun","planets","mercury","earth","galaxy","meteroids","blackholes"]
+
+numbers=[23, 43, 32, 9, 0, 15, 27, 41]
+
+
+newCopyWords= words.copy()
+print("Copied ",newCopyWords)
+
+newCopyNumbers= numbers.copy()
+print("Copied ", newCopyNumbers)
