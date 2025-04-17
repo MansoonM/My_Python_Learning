28cf35a1590bed97ce528c867315a4e0992321dd
Mansoon Mohanty <136214687+MansoonM@users.noreply.github.com>
2025-04-17 07:43:35 +0530

    Add files via upload

---
+++ 23_tuple.py
@@ -0,0 +1,16 @@
+# tuple
+tuple1=(2,5,8,22)
+
+# print the tuple
+print(tuple1)
+
+# data type
+print(type(tuple1))
+
+#  print each element using for loop
+for x in tuple1:
+    print(x)
+
+# accessing items in a tuple
+print("------last element-------")
+print(tuple1[-1])
