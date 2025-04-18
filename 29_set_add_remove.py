1385ed527a100e53845fa63a5f72bcb39b408e46
Mansoon Mohanty <136214687+MansoonM@users.noreply.github.com>
2025-04-18 06:56:22 +0530

    Add files via upload

---
+++ 29_set_add_remove.py
@@ -0,0 +1,17 @@
+# add a number
+set1={23,4,6,2,46}
+set1.add(9)
+print(set1)
+
+# add a new fruit
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset)
+
+# remove a number
+set1.remove(9)
+print(set1)
+
+# remove a fruit
+thisset.remove("cherry")
+print(thisset)
