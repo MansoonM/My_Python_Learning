8bf6cfb055336c99efb546d6b606cd3201363283
Mansoon Mohanty <136214687+MansoonM@users.noreply.github.com>
2025-04-18 06:55:46 +0530

    Add files via upload

---
+++ 26_tuple_loop.py
@@ -0,0 +1,10 @@
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+  print(x)
+
+# ============
+print("       ")
+# ==========
+thistuple1 = ("apple", "banana", "cherry")
+for i in range(len(thistuple1)):
+  print(thistuple1[i])
