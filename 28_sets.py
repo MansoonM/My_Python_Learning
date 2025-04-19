b6315a05db87017c03e8bdc15eee2efbee280a2b
Mansoon Mohanty <136214687+MansoonM@users.noreply.github.com>
2025-04-19 07:10:38 +0530

    Add files via upload

---
+++ 28_sets.py
@@ -0,0 +1,12 @@
+set1= {"apple","banana","kiwi"}
+print(set1)
+print(type(set1))
+
+
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+  print(x)
+
+# boolean output
+print("banana" in thisset)
+print("banana" not in thisset)
