371f2eeede9d3df21fbe3491a23540ba2fe5c26e
Mansoon Mohanty <136214687+MansoonM@users.noreply.github.com>
2025-04-19 07:11:31 +0530

    Add files via upload

---
+++ 31_set_join.py
@@ -0,0 +1,20 @@
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+
+
+# using union
+set3 = set1.union(set2)
+print(set3)
+
+
+# space
+print("    ")
+
+# new method
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = {"John", "Elena"}
+set4 = {"apple", "bananas", "cherry"}
+
+myset = set1 | set2 | set3 |set4
+print(myset)
