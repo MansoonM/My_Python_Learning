f63e6af336dc912a6fcf4b03a534d583d1203a95
Mansoon Mohanty <136214687+MansoonM@users.noreply.github.com>
2025-04-20 06:40:42 +0530

    Add files via upload

---
+++ 32_dict.py
@@ -0,0 +1,32 @@
+# dictionaries
+# key : value
+
+this_dict={
+    "name": "Mansoon Mohanty",
+    "roll no": 5,
+    "branch": "cse"
+}
+
+print(this_dict)
+print(type(this_dict))
+print(this_dict["name"])
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+print(thisdict)
+print(thisdict["brand"])
+print(len(thisdict))
+
+
+
+# There are four collection data types in the Python programming language:
+
+# List is a collection which is ordered and changeable. Allows duplicate members. 
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+# Dictionary is a collection which is ordered** and changeable. No duplicate members.
