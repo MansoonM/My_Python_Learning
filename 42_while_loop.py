daa82888da365d06bc898c8b27e7e65604f45c9c
Mansoon Mohanty <136214687+MansoonM@users.noreply.github.com>
2025-04-23 07:08:36 +0530

    Add files via upload

---
+++ 42_while_loop.py
@@ -0,0 +1,23 @@
+number=1
+
+while number<=10:
+    print("Hi", number)
+    number=number+1
+
+print("    ") # for space
+
+num=1
+
+while num<10:
+    print("now num is ",num)
+    num=num+1
+
+
+print("    ") # for space
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
