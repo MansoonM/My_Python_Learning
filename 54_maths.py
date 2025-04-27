ba3341777593409a175682208e1deaa6ce5d1b0a
Mansoon Mohanty <136214687+MansoonM@users.noreply.github.com>
2025-04-27 07:39:59 +0530

    Add files via upload

---
+++ 54_maths.py
@@ -0,0 +1,37 @@
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+
+# The abs() function returns the absolute (positive) value of the specified number:
+
+a = abs(-7.25)
+print(a)
+
+#  4 to the power 3 or 4*4*4
+b = pow(4, 3)
+print(b)
+
+
+
+# =============================?math module
+
+import math
+
+a = math.sqrt(64)
+
+print(a)
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
+
+
+
+
+z = math.pi
+print(z)
